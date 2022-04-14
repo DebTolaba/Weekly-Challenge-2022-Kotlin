0c0e05c06d48288df2737d4637e509f55e2fcfe5
DebTolaba <deb.tolaba@gmail.com>
2022-04-14 20:20:09 -0300

    Solución Challenge15.kt

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge15.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge15.kt
@@ -21,3 +21,102 @@ package com.mouredev.weeklychallenge2022
  *
  */
 
+fun main(){
+    calculateDays("15/12/2021", "27/01/2022") // 43 dias
+    calculateDays("07/07/2022", "29/07/2022") // 22 dias
+    calculateDays("17/01/2020","17/01/2020") // 0 dias
+    calculateDays("01/07/2022","01/12/2022") // 153 dias
+    calculateDays("03/01/2020","17/08/2024") // 1688 dias
+    calculateDays("03/01/2020","") // fecha incorrecta
+    calculateDays("2022/01/01","17/08/2024") // fecha incorrecta
+    calculateDays("  /  /   ","17/08/2024") // fecha incorrecta
+    calculateDays("03/01/2020","dd/Ñm/202L") // fecha incorrecta
+}
+
+fun calculateDays(date1: String, date2: String){
+    val time = Time()
+    if(time.isDate(date1) and time.isDate(date2)){
+        val days = time.getDays(time.getDate(date1), time.getDate(date2))
+        println("Cantidad de dias: $days") }
+
+    else println("Error: fecha incorrecta")
+}
+
+data class Date(var day: Int, var month: Int, var year: Int)
+
+class Time{
+    private val daysOfMonth: MutableMap<Int, Int> = mutableMapOf(
+        1 to 31, 2 to 28, 3 to 31, 4 to 30, 5 to 31, 6 to 30,
+        7 to 31, 8 to 31, 9 to 30, 10 to 31, 11 to 30, 12 to 31)
+    private var daysYear = 365
+
+    fun isDate(sdate: String): Boolean{
+        val format = Regex("^\\d{2}/\\d{2}/\\d{4}$")
+        if(format.matches(sdate)) {
+            val list = sdate.split("/").map { it.toInt() }
+            val date = Date(list[0], list[1], list[2])
+
+            if (date.month in 1..12) {
+                var lim = daysOfMonth[date.month]
+                if((date.month == 2) and (isBisiesto(date.year))) { lim = 29 }
+                if((date.day in 1..lim!!)) return true
+            }
+        }
+        return false
+    }
+
+    fun getDate(date: String): Date{
+        val list = date.split("/").map { it.toInt() }
+        return Date(list[0], list[1], list[2])
+    }
+
+    fun getDays(date1: Date, date2: Date): Int{
+        val (dateStart, dateEnd) = orderDates(date1,date2)
+
+        var days = diffYears(dateStart.year , (dateEnd.year - dateStart.year))
+        days += restOfYear(dateStart.month, dateStart.year) - dateStart.day
+        days -= restOfYear(dateEnd.month, dateEnd.year) - dateEnd.day
+        return days
+    }
+
+    private fun diffYears(yearStart: Int, diff: Int): Int{
+        var days = 0
+        for(i in 1..diff){
+            days += if(isBisiesto(yearStart + i)) 366 else 365
+        }
+        return days
+    }
+
+    private fun restOfYear(monthStart: Int, year:Int): Int{
+        var days = 0
+
+        if(isBisiesto(year)){  daysYear = 366; daysOfMonth[2] = 29 }
+        else{ daysYear = 365; daysOfMonth[2] = 28 }
+
+        if(monthStart == 1) return daysYear
+        else{
+            for(month in monthStart..12){
+                days += daysOfMonth[month]!!
+            }
+        }
+
+        return days
+    }
+
+    private fun orderDates(date1: Date, date2: Date): Pair<Date, Date>{
+        var order = Pair(date1,date2)
+        if(date1.year != date2.year){
+            if(date1.year > date2.year) order = Pair(date2, date1) }
+
+        else if(date1.month != date2.month){
+            if(date1.month > date2.month) order = Pair(date2, date1) }
+
+        else if(date1.day > date2.day) order = Pair(date2, date1)
+
+        return order
+    }
+
+    private fun isBisiesto(year: Int): Boolean {
+        return ((year % 4 == 0) and ((year % 100 != 0) or (year % 400 == 0)))
+    }
+}
